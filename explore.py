7e5e8dd7e80a8cfe1e63a14e31c6fdeeb356547d
JADEBIT <72592284+Jadebit-li@users.noreply.github.com>
2026-06-29 17:36:08 +0530

    split for test and train

--- explore.py
+++ explore.py
@@ -1,11 +1,12 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("data/payload_full.csv")
-# print(df.shape)
-# print(df['label'].value_counts())
-# print(df['attack_type'].value_counts())
-# df['is_malicious'] = df['attack_type'].apply(lambda x: 0 if x == 'norm' else 1)
-# print(df['is_malicious'].value_counts())
+print(df.shape)
+print(df['label'].value_counts())
+print(df['attack_type'].value_counts())
+df['is_malicious'] = df['attack_type'].apply(lambda x: 0 if x == 'norm' else 1)
+print(df['is_malicious'].value_counts())
 
 print("Total rows:", len(df))
 print("Duplicate payloads:", df['payload'].duplicated().sum())
@@ -18,3 +19,19 @@ print("Missing payloads:", df['payload'].isna().sum())
 print(df['payload'].str.len().describe())
 
 print(df.loc[df['payload'].str.len().idxmax(), ['payload', 'attack_type']])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    df['payload'],
+    df['is_malicious'],
+    test_size = 0.2,
+    stratify = df['attack_type'],
+    random_state = 42
+)
+
+print ("Train size : ", len(X_train))
+print ("Test size : ", len(X_test))
+print (y_train.value_counts(normalize = True))
+print (y_test.value_counts(normalize = True))
+
+print(X_train.iloc[0])
+print(y_train.iloc[0])
